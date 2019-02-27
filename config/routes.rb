696224ff97784118e96d1eb48b0fceacb98f60f8
大久保美涼 <okubomisuzu@ookubomisuzunoMacBook-Pro.local>
2019-02-27 20:50:59 +0900

    Add Title

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'static_page/help'
   
+  get 'static_page/about'
+  
   root 'applicateion#hello'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
